fd7ece82da0347583d8693d088dab39ce9140dc7
yungshun317 <seraph317@gmail.com>
2021-06-01 11:27:14 +0800

    [FEAT] Add the `createArray` function

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-	return [
-	    <FaStar color="red" />,
-	    <FaStar color="red" />,
-	    <FaStar color="red" />,
-	    <FaStar color="grey" />,
-	    <FaStar color="grey" />
-	];
+// [2] Create a new array from the given length
+const createArray = length => [...Array(length)];
+
+// [1] StarRating component
+export default function StarRating({ totalStars = 5 }) {
+	return createArray(totalStars).map((n, i) => <Star key={i} />);
 }
