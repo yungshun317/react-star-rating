53663761b94443a944677182d78cdf8419bcd567
yungshun317 <seraph317@gmail.com>
2021-06-01 10:48:51 +0800

    [CHORE] Configure webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: {
+		index: path.resolve(__dirname, "src", "index.js")
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		sourceMapFilename: "bundle.map"
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+		    {
+		        test: /\.js$/i,
+		        exclude: /node_modules/,
+		        use: ["babel-loader"]
+		    }
+		]
+	},
+	plugins: [
+        new HtmlWebpackPlugin({
+        	template: path.resolve(__dirname, "src", "index.html")
+        })
+	]
+};
