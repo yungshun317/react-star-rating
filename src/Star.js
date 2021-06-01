7f0fc41d116761a3f7ab36617b9150c3325a7cca
yungshun317 <seraph317@gmail.com>
2021-06-01 19:37:20 +0800

    [FEAT] Make the stars clickable

--- src/Star.js
+++ src/Star.js
@@ -1,3 +1,6 @@
-const Star = ({ selected = false }) => (
-	<FaStar color={selected ? "red" : "grey"} />
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false, onSelect = f => f }) => (
+	<FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
--- src/StarRating.js
+++ src/StarRating.js
@@ -4,15 +4,20 @@ import { FaStar } from "react-icons/fa";
 // [2] Create a new array from the given length
 const createArray = length => [...Array(length)];
 
-// [1] StarRating component
+// [1] `StarRating` component
 export default function StarRating({ totalStars = 5 }) {
-	// `selectedStars` initially is set to `3`
-	const [selectedStars] = useState(3);
+	// `selectedStars` initially is set to `0`;
+	// function `setSelectedStar` is used to change the state value
+	const [selectedStars, setSelectedStars] = useState(0);
 
 	return (
 		<>
 		    {createArray(totalStars).map((n, i) => (
-		    	<Star key={i} selected={selectedStars > i} />
+		    	<Star 
+		    	    key={i} 
+		    	    selected={selectedStars > i}
+		    	    onSelect={() => setSelectedStars(i + 1)} 
+		    	/>
 		    ))}
 		    <p>
 		        {selectedStars} of {totalStars} stars
